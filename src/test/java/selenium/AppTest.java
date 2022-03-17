95d4c853eb3560cf6d1d1a86057982e6cc88deaf
Victor Menares <vmenares@scj.gob.cl>
2022-03-17 11:11:19 -0300

    2 Navegadores

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -18,6 +18,7 @@ public class AppTest
     
     private WebDriver driver;
 
+    /*
     @Before
     public void setUp(){
         System.out.println("Iniciando configuración...");
@@ -29,7 +30,9 @@ public class AppTest
         System.out.println(driver.getTitle());
         //driver.navigate().to("https://www.google.com");
     }
+     */
 
+     /*
     @Test
     public void shouldAnswerWithTrue()
     {
@@ -43,7 +46,46 @@ public class AppTest
 
 
     }
+    */
 
 
+    @Test
+    public void buscarAmazon(){
+        System.out.println("Iniciando configuración...");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        driver.manage().window().maximize();
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+        System.out.println("Iniciando Pruebas...");
+        WebElement searchbox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchbox.sendKeys("The Phoenix Project");
+        searchbox.submit();
+        System.out.println("driver.getTitle() : " + driver.getTitle());
+        assertEquals("Amazon.com : The Phoenix Project", driver.getTitle());
+    }
+
+    @Test
+    public void buscarGoogle(){
+        System.out.println("Iniciando configuración...");
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());
+        //driver.navigate().to("https://www.google.com");
+        System.out.println("Iniciando Pruebas...");
+        WebElement searchbox = driver.findElement(By.name("q"));
+        searchbox.sendKeys("HandBook Devops");
+        searchbox.submit();
+        WebElement searchbox1 = driver.findElement(By.cssSelector(".sbre .wM6W7d"));
+        searchbox1.submit();
+
+        System.out.println("driver.getTitle() : " + driver.getTitle());
+        assertEquals("HandBook Devops - Buscar con Google", driver.getTitle());
+        //assertEquals("Amazon.com : The Phoenix Project", driver.getTitle());
+    }
 
 }
